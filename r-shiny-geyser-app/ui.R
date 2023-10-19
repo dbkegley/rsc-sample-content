cc17ac151e94d8fc14d175c39a20f5141d4de563
David Kegley <david.kegley@posit.co>
2023-10-19 12:25:26 -0400

    add update6

--- r-shiny-geyser-app/ui.R
+++ r-shiny-geyser-app/ui.R
@@ -13,7 +13,7 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data (update5)"),
+    titlePanel("Old Faithful Geyser Data (update6)"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
