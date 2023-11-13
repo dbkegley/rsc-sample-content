8b028d6ef08c7e6357511cc1ea22e2ea7788df87
David Kegley <david.kegley@posit.co>
2023-11-13 12:06:47 -0500

    remove other content

--- custom-images/reticulated-parameterized-rmd/README.md
+++
@@ -1,1 +0,0 @@
-A simple parameterized report including reticulate (python) content, used for launch testing.
--- custom-images/reticulated-shiny-app/server.py
+++
@@ -1,4 +0,0 @@
-import random
-
-# Generate 100 random numbers between 1 and 100
-data = random.sample(range(0, 100), 100)
