e5c9ff5207c38b6e36ca3b27d68a4057fda9d83f
David Kegley <david.kegley@posit.co>
2024-07-22 11:17:47 -0400

    update oauth test app to show parsed and raw session-token

--- local/python-oauth-integration-app/app.py
+++ local/python-oauth-integration-app/app.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 # mypy: ignore-errors
 import json 
+import sys
 
 import jwt
 from posit.connect.client import Client
 from shiny import App, Inputs, Outputs, Session, render, ui
 
+style = 'background-color: #f6f6f6; overflow: auto; white-space: pre-wrap; overflow-wrap: anywhere; width: 60%;'
 app_ui = ui.page_fluid(
-        ui.row(ui.output_text_verbatim("text"),
-               style='background-color: #f6f6f6; overflow: auto; white-space: pre-wrap; overflow-wrap: anywhere; height: 25em; width: 60%'))
+    ui.row(ui.h3("Session Token")),
+    ui.row(ui.output_text_verbatim("raw_session_token_text"), style=style),
+    ui.row(ui.output_text_verbatim("parsed_session_token_text"), style=style+'height: 12em;'),
+    ui.row(ui.span(style="padding: 1em")),
+    ui.row(ui.h3("Access Token")),
+    ui.row(ui.output_text_verbatim("raw_access_token_text"), style=style),
+    ui.row(ui.output_text_verbatim("parsed_access_token_text"), style=style+'height: 12em;')
+)
 
 def server(input: Inputs, output: Outputs, session: Session):
     """
@@ -20,15 +28,33 @@ def server(input: Inputs, output: Outputs, session: Session):
         "Posit-Connect-User-Session-Token"
     )
 
-    @render.text
-    def text():
-        with Client() as client:
+    with Client() as client:
+        try:
             credentials = client.oauth.get_credentials(session_token)
-            try:
-                token = jwt.decode(jwt=credentials.get("access_token"), options={"verify_signature": False})
-                return json.dumps(token, indent=4) 
-            except jwt.exceptions.DecodeError:
-                return credentials.get("access_token", "Access Token Not Found")
+        except Exception as e:
+            print(f"Unable to fetch credentials: %s" % e, file=sys.stderr)
+            credentials = dict()
+
+    @render.text
+    def raw_session_token_text():
+        return session_token
+
+    @render.text
+    def parsed_session_token_text():
+        token = jwt.decode(jwt=session_token, options={"verify_signature": False})
+        return json.dumps(token, indent=4) 
+
+    @render.text
+    def raw_access_token_text():
+        return credentials.get("access_token", "Access Token Not Found")
+
+    @render.text
+    def parsed_access_token_text():
+        try:
+            token = jwt.decode(jwt=credentials.get("access_token"), options={"verify_signature": False})
+            return json.dumps(token, indent=4) 
+        except jwt.exceptions.DecodeError:
+            return credentials.get("access_token", "Access Token Not Found")
 
 
 app = App(app_ui, server)
